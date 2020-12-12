8250946285520b0d7765e1b614d10b2c661165a2
YaniPrieto <prietojohnchristian@gmail.com>
2020-12-12 08:30:46 +0800

    Part 1 Done

---
+++ tempData.js
@@ -0,0 +1,38 @@
+export default tempData = [
+    {
+        name: "Coffee Date",
+        color: "#24A6d9",
+        todos: [
+            {
+                title: "Coffee Date part 1",
+                completed: false
+            },
+            {
+                title: "Dentist Checkup",
+                completed: true
+            },
+            {
+                title: "Clean house",
+                completed: false
+            },
+        ]
+    },
+    {
+        name: "Finals Exam Deadline",
+        color: "#8022d9",
+        todos: [
+            {
+                title: "Study Programming",
+                completed: false
+            },
+            {
+                title: "Study Life",
+                completed: false
+            },
+            {
+                title: "Study History",
+                completed: false
+            },
+        ]
+    },
+]
